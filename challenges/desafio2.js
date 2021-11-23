d1c981f62895b7b3a255bc80c3539383539af8d9
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-23 14:43:07 -0300

    [requisito 2] resolucao

---
+++ challenges/desafio2.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { valorUnitario: { $exists: false } },
+  { $set: { valorUnitario: NumberDecimal("0.00")  } },
+);
+
+db.produtos.find(
+    {},
+    { nome: true, valorUnitario: true, _id: false },
+);
