03e5243fc89780c8b95ee9e96243e06157a79d48
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-23 15:35:28 -0300

    [requisito 5] resolucao

---
+++ challenges/desafio5.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany(
+  { nome: { $not: { $eq: "McChicken" } } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
+
+db.produtos.find(
+    { },
+    { nome: true, ingredientes: true , _id: false },
+);
