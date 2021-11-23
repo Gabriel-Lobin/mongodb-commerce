30d6eea89b9443b64a89eb24c8690a6b1327b039
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-11-23 16:48:11 -0300

    [requisito 10] correcao

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -5,12 +5,12 @@ db.produtos.updateMany(
 
 db.produtos.updateOne(
   { nome: "Big Mac" },
-  { $inc: { "vendasPorDia.4": 60 } },
+  { $inc: { "vendasPorDia.3": 60 } },
 );
 
 db.produtos.updateMany(
   { tags: { $in: ["bovino", "pão"] } },
-  { $inc: { "vendasPorDia.7": 120 } },
+  { $inc: { "vendasPorDia.6": 120 } },
 );
 
 db.produtos.find(
